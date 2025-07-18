1907874bbfebcf17c731ae646c716a96e465cb54
Dhaval Gohel <dhavalgohel1107@gmail.com>
2025-07-18 20:47:05 +0530

    final carry look ahead adder

--- carry_look_ahead_adder.v
+++ carry_look_ahead_adder.v
@@ -1,7 +1,7 @@
 module cla_adder #(parameter N = 16)(
     input  signed [N-1:0] a, b,
     output signed [N-1:0] sum,
-    output [4:0] flags  // {V, C, N, Z, P}
+    output [3:0] flags  // {V, N, Z, P}
 );
 
     wire [N-1:0] g, p;
@@ -19,19 +19,39 @@ module cla_adder #(parameter N = 16)(
         end
     endgenerate
 
+    wire zero_flag     = (sum == 0);
+    wire negative_flag = sum[N-1];
+    wire overflow_flag = (a[N-1] & b[N-1] & ~sum[N-1]) |
+                         (~a[N-1] & ~b[N-1] & sum[N-1]);
+    wire parity_flag   = ~^sum; //even parity
 
-    wire zero_flag  = (sum == 0);
-    wire negative_flag = (sum[15] == 1);
-    wire carry_flag    = carry[N];
-
-    // Overflow if: operands have same sign, result has opposite sign
-    wire overflow_flag = (~a[N-1] & ~b[N-1] & sum[N-1]) | 
-                         ( a[N-1] &  b[N-1] & ~sum[N-1]);
-
-    // Parity Flag: 1 if even parity (even number of 1s)
-    wire parity_flag = ~^sum;  
-
-    assign flags = {overflow_flag, carry_flag, negative_flag, zero_flag, parity_flag};
+    assign flags = {overflow_flag, negative_flag, zero_flag, parity_flag};
 
 endmodule
 
+
+module cla_tb;
+    parameter N = 16;
+
+    reg  signed [N-1:0] a, b;
+    wire signed [N-1:0] sum;
+    wire [3:0] flags; // {V, N, Z, P}
+
+    integer i;
+
+    cla_adder #(.N(N)) uut (.a(a), .b(b), .sum(sum), .flags(flags));
+
+    initial begin
+        for (i = 0; i < 6; i = i + 1) begin
+            a = $random; 
+            b = $random; 
+            #5;
+
+            $display("Test %0d: a = %0d, b = %0d -> sum = %0d (%016b) | Flags = {V=%b, N=%b, Z=%b, P=%b}",
+                i+1, a, b, sum, sum,
+                flags[3], flags[2], flags[1], flags[0]);
+        end
+
+        $finish;
+    end
+endmodule
